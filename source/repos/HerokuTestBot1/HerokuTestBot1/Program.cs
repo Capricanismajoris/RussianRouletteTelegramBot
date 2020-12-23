51b702de0b594113eae72dc385ee5107da4eb5bf
Capricanismajoris <capricavoid7@gmail.com>
2020-12-23 23:21:58 +0200

    init

---
+++ source/repos/HerokuTestBot1/HerokuTestBot1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace HerokuTestBot1
+{
+    class Program
+    {
+        static TelegramBotClient Bot;
+        static readonly string token = "1448082591:AAGiW_-38djbQ4Jd-KjZ4STzoy905qB9v10";
+
+        static void Main()
+        {
+            Bot = new TelegramBotClient(token);
+
+            Bot.OnMessage += OnMessage;
+
+            Bot.StartReceiving();
+            Console.WriteLine("start");
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        static async void OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message == null) return;
+
+            await Bot.SendTextMessageAsync(
+                chatId: e.Message.Chat.Id,
+                text: "дарова"
+                );
+        }
+    }
+}
